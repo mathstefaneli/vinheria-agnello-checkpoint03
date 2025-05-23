f1cb3b737e2d9b7dc9896ca3f4ba33d8aef62133
Matheus Gomes <mathstefaneli@gmail.com>
2025-05-23 09:43:32 -0300

    array inicial de vinhos

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const vinhos = [
+    {nome: "Vinho Arinto", tipo: "branco", safra: 2021, estoque: 5},
+    {nome: "Merlot", tipo: "tinto", safra: 2012, estoque: 10},
+    {nome: "Malbec", tipo: "tinto", safra: 2017, estoque: 3},
+    {nome: "Lisbonita Rose", tipo: "rose", safra: 2022, estoque: 8}
+]
