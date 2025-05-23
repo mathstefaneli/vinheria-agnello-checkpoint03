3036c3738ad6f5ae73ffd12392f5ca129239a6ad
Matheus Gomes <mathstefaneli@gmail.com>
2025-05-23 09:49:28 -0300

    função listar os vinhos

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,15 @@ const vinhos = [
     {nome: "Merlot", tipo: "tinto", safra: 2012, estoque: 10},
     {nome: "Malbec", tipo: "tinto", safra: 2017, estoque: 3},
     {nome: "Lisbonita Rose", tipo: "rose", safra: 2022, estoque: 8}
-]
+];
 
 function adicionarVinho(nome, tipo, safra, estoque) {
   vinhos.push({ nome, tipo, safra, estoque });
+}
+
+function listarVinhos() {
+  console.log("🍷 Lista de vinhos:");
+  vinhos.forEach((vinho, index) => {
+    console.log(`${index + 1}. ${vinho.nome} (${vinho.tipo}, ${vinho.safra}) - Estoque: ${vinho.estoque}`);
+  });
 }
